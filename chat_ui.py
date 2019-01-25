a3414e1caaa7714f541fce56ebdd4f7ed587efb1
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-01-25 15:07:56 +1100

    chat app working

--- chat_ui.py
+++ chat_ui.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QLabel, QPushButton, QLineEdit, QTextEdit
 from PyQt5.QtGui import QPalette
 
 class ChatUI():
@@ -9,6 +10,13 @@ class ChatUI():
 
         # counter for number of clicks
         self.button_clicks = 0
+
+
+        initial_history = """Welcome to Hackchat
+
+Batman: Hi there
+You: What's up?
+Batman: Crime"""
 
         # Create a GUI application
         app = QApplication([])
@@ -26,26 +34,49 @@ class ChatUI():
         layout = QVBoxLayout()
         window.setLayout(layout)
 
+        
+
+
+
+        # NAMING CONVETION
+        # txt_ is a multi line text box
+        # inp_ is an input box
+        # btn_ is a button
+        # lbl_ is a label
+
+        txt_history = QTextEdit()
+        txt_history.setPlainText(initial_history)
+        txt_history.setReadOnly(True)
+
         # Create a text display
-        label = QLabel('Hello, Cyber')
+        lbl_message = QLabel('Type your message:')
+
+        # Create an input box
+        inp_message = QLineEdit()
+
+        inp_message.returnPressed.connect(self.send)
+        
 
         # Create a button
-        button = QPushButton('Click me!')
-
-        
-        button.clicked.connect(self.button_clicked)
+        #button = QPushButton('Click me!')
+        #button.clicked.connect(self.button_clicked)
 
         # Add widgets to the layout
-        layout.addWidget(label)
-        layout.addWidget(button)
+        #layout.addWidget(button)
+        layout.addWidget(txt_history)
+
+        layout.addWidget(lbl_message)
+        layout.addWidget(inp_message)
+
 
         window.show()
 
         self.app = app
         self.window = window
         self.layout = layout
-        self.label = label
-        self.button = button
+        self.inp_message = inp_message
+        self.txt_history = txt_history
+        #self.button = button
 
     def run(self):
         # Enter the application's main loop
@@ -53,6 +84,16 @@ class ChatUI():
         self.app.exec_()
 
         print("Application was closed")
+
+    def send(self):
+        user_typed = self.inp_message.text()
+
+        # add "You: " and put it in display window
+        display = "You: " + user_typed
+        self.txt_history.append(display)
+
+        # make the input box blank again
+        self.inp_message.setText(None)
 
     def button_clicked(self):
         self.button_clicks += 1
